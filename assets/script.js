4e5b60fd66fc57b87907ec8bef068b4b07e0b067
DuckTurtle <qschnell04@gmail.com>
2023-03-30 13:33:03 -0500

    fixed generation bug

--- assets/script.js
+++ assets/script.js
@@ -6,59 +6,59 @@ var saveButton = document.querySelector("#saveScores");
 var playagain = document.querySelector("#playAgainBnt");
 var timer;
 var timerTime;
-var questionTextBox = document.querySelector(".Questionblank");
-var bluffAnswers =["jQuery","else","Local Storage","function","keydown/keyup","if","else/else if",".querySelector","True","Array","Yes","for","script","Null","No","probably a cranberry","variable","Robot","Next Question"];
+var questionTextBox = document.querySelector(".startPageText");
+var bluffAnswers = ["jQuery", "else", "Local Storage", "function", "keydown/keyup", "if", "else/else if", ".querySelector", "True", "Array", "Yes", "for", "script", "Null", "No", "probably a cranberry", "variable", "Robot", "Next Question"];
 var questionAnswers = {
- 
+
     option1: {
-        question:"Every else statment you need a ____ statment before.",
+        question: "Every else statment you need a ____ statment before.",
         answer: "if"
     },
     option2: {
-        question:"This exacutes a section of code every time its called?",
+        question: "This exacutes a section of code every time its called?",
         answer: "function"
     },
     option3: {
-        question:"What is one thing that makes javascript better?",
+        question: "What is one thing that makes javascript better?",
         answer: "jQuery"
     },
     option4: {
-        question:"Before a when statment there must be a ___ statment",
+        question: "Before a when statment there must be a ___ statment",
         answer: "for"
     },
     option5: {
-        question:"A variable in a function can be used by another Function.",
-        answer:"False"
+        question: "A variable in a function can be used by another Function.",
+        answer: "False"
     },
     option6: {
-        question:"The element to call your javascript file in html is a _____ Element",
+        question: "The element to call your javascript file in html is a _____ Element",
         answer: "script"
     },
     option7: {
-        question:"A variable with multiple outputs is called an ___?",
+        question: "A variable with multiple outputs is called an ___?",
         answer: "Array"
     },
     option8: {
-        question:"This can be used to store data on the users device.",
+        question: "This can be used to store data on the users device.",
         answer: "Local Storage"
     },
     option9: {
-        question:"This can be used to help track the key presses of a user.",
+        question: "This can be used to help track the key presses of a user.",
         answer: "keydown/keyup"
     },
     option10: {
-        question:"Dex is probably hiding the fact that he is a_______.",
+        question: "Dex is probably hiding the fact that he is a_______.",
         answer: "Robot"
     },
 };
 var removedQuestions = "";
 var questionsLeft;
 
-function init(){
+function init() {
     getOldScores();
 };
 
-function startGame(){
+function startGame() {
     timerCount = 50;
     questionsLeft = 5;
     choseOption();
@@ -66,61 +66,61 @@ function startGame(){
     hideExtra();
 };
 /* This resests the questions list, part of my system to prevent repete questions*/
-function resetAray(){
-     questionAnswers = {
-     option1: {
-        question:"Every else statment you need a ____ statment before.",
-        answer: "if"
-    },
-    option2: {
-        question:"This exacutes a section of code every time its called?",
-        answer: "function"
-    },
-    option3: {
-        question:"What is one thing that makes javascript better?",
-        answer: "jQuery"
-    },
-    option4: {
-        question:"Before a when statment there must be a ___ statment",
-        answer: "for"
-    },
-    option5: {
-        question:"A variable in a function can be used by another Function.",
-        answer: "False"
-    },
-    option6: {
-        question:"The element to call your javascript file in html is a _____ Element",
-        answer: "script"
-    },
-    option7: {
-        question:"A variable with multiple outputs is called an ___?",
-        answer: "Array"
-    },
-    option8: {
-        question:"This can be used to store data on the users device.",
-        answer: "Local Storage"
-    },
-    option9: {
-        question:"This can be used to help track the key presses of a user.",
-        answer: "keydown/keyup"
-    },
-    option10: {
-        question:"Dex is probably hiding the fact that he is a_______.",
-        answer: "Robot"
-    },
+function resetAray() {
+    questionAnswers = {
+        option1: {
+            question: "Every else statment you need a ____ statment before.",
+            answer: "if"
+        },
+        option2: {
+            question: "This exacutes a section of code every time its called?",
+            answer: "function"
+        },
+        option3: {
+            question: "What is one thing that makes javascript better?",
+            answer: "jQuery"
+        },
+        option4: {
+            question: "Before a when statment there must be a ___ statment",
+            answer: "for"
+        },
+        option5: {
+            question: "A variable in a function can be used by another Function.",
+            answer: "False"
+        },
+        option6: {
+            question: "The element to call your javascript file in html is a _____ Element",
+            answer: "script"
+        },
+        option7: {
+            question: "A variable with multiple outputs is called an ___?",
+            answer: "Array"
+        },
+        option8: {
+            question: "This can be used to store data on the users device.",
+            answer: "Local Storage"
+        },
+        option9: {
+            question: "This can be used to help track the key presses of a user.",
+            answer: "keydown/keyup"
+        },
+        option10: {
+            question: "Dex is probably hiding the fact that he is a_______.",
+            answer: "Robot"
+        },
     };
 
 };
 
-function win(){
+function win() {
     saveScore();
 };
-function startTimer(){
-    timer = setInterval(function(){
+function startTimer() {
+    timer = setInterval(function () {
         timerCount--;
         timerEl.textContent = "Time Left: " + timerCount;
-        if(timerCount >= 0){
-            if(questionsLeft === 0 && timerCount > 0){
+        if (timerCount >= 0) {
+            if (questionsLeft === 0 && timerCount > 0) {
                 clearInterval(timer);
                 win();
             };
@@ -129,87 +129,87 @@ function startTimer(){
             clearInterval(timer);
             lose();
         };
-    },1000);
+    }, 1000);
 };
-function choseOption(){
-     var choseQuestion = Object.values(questionAnswers);
-     choseQuestion = choseQuestion[Math.floor(Math.random()* choseQuestion.length)];
-     var currentQuestion = choseQuestion.question;
-     var currentAnswer = choseQuestion.answer;
-    var questionText = document.createElement("h2");
-    document.getElementById("questions").appendChild(questionText);
-    questionText.textContent = `${currentQuestion}`;
+var previousQuestion;
+var questionText = document.createElement("h2");
+questionText.setAttribute("id", "questionTextBox");
+document.getElementById("questions").appendChild(questionText);
+function choseOption() {
+    var choseQuestion = Object.values(questionAnswers);
+    choseQuestion = choseQuestion[Math.floor(Math.random() * choseQuestion.length)];
+    if (previousQuestion === choseQuestion) {
+        choseQuestion = choseQuestion[Math.floor(Math.random() * choseQuestion.length)];
+    }
+    else if (previousQuestion !== choseQuestion) {
+        var currentQuestion = choseQuestion.question;
+        console.log(choseQuestion);
+        var currentAnswer = choseQuestion.answer;
+    };
+    document.getElementById("questionTextBox").textContent = `${currentQuestion}`;
     delete questionAnswers.choseQuestion;
-    var answeroptions = ['"' + genbluff(1)+'"','"'+ genbluff(1)+'"', '"'+ currentAnswer +'"','"'+ genbluff(1)+'"'];
+    previousQuestion = choseQuestion;
+    var answeroptions = ['' + genbluff(1) + '', '' + genbluff(1) + '', '' + currentAnswer + '', '' + genbluff(1)];
     Array(answeroptions);
-    console.log(answeroptions);
     resetBluff();
-     var aO1 = document.createElement("button");
-     var aO2 = document.createElement("button");
-     var aO3 = document.createElement("button");
-     var aO4 = document.createElement("button");
-    function stringAnswers(){
-        var answerScrambler = answeroptions[Math.floor(Math.random()*answeroptions.length)];
-        delete answeroptions.answerScrambler;
-        console.log(answerScrambler);
-        return(answerScrambler);
-    }; /*if statment here to seprate variables*/
-    var op1 = stringAnswers();
-    var op1 = stringAnswers();
-    var op1 = stringAnswers();
-    var op1 = stringAnswers();
-    aO1.textContent = op1;
-    aO2.textContent = op2
-    aO3.textContent = op3
-    aO4.textContent = op4;
-    document.getElementById("questions").appendChild(aO1);
-    document.getElementById("questions").appendChild(aO2);
-    document.getElementById("questions").appendChild(aO3);
-    document.getElementById("questions").appendChild(aO4);
-    resetAnswerString()
-    function genbluff(len){
-        var output = "";
-        for (let i = 0; i < len; i++) {
-            output += " " + bluffAnswers[Math.floor(Math.random()*bluffAnswers.length)] + "";
+    var aO1 = document.createElement("button");
+    var aO2 = document.createElement("button");
+    var aO3 = document.createElement("button");
+    var aO4 = document.createElement("button");
+    function stringAnswers() {
+        var answerScrambler = answeroptions[Math.floor(Math.random() * answeroptions.length)];
+        const index = answeroptions.indexOf(answerScrambler);
+        if (index > -1) { 
+         answeroptions.splice(index, 1); 
         }
-        console.log(output);
-        if (output == currentAnswer){
-            output -= " " + currentAnswer + ""
-            output += " " + bluffAnswers[Math.floor(Math.random()*bluffAnswers.length)]  + "";
-        };
-        delete bluffAnswers.output;
-        return (output);
-        
+            return (answerScrambler);
+}
+aO1.textContent = stringAnswers();
+aO2.textContent = stringAnswers();
+aO3.textContent = stringAnswers();
+aO4.textContent = stringAnswers();
+document.getElementById("questions").appendChild(aO1);
+document.getElementById("questions").appendChild(aO2);
+document.getElementById("questions").appendChild(aO3);
+document.getElementById("questions").appendChild(aO4);
+function genbluff(len) {
+    var output = "";
+    for (let i = 0; i < len; i++) {
+        output += " " + bluffAnswers[Math.floor(Math.random() * bluffAnswers.length)] + "";
+    }
+    if (output == currentAnswer) {
+        output -= " " + currentAnswer + ""
+        output += " " + bluffAnswers[Math.floor(Math.random() * bluffAnswers.length)] + "";
     };
-    function resetAnswerString(){
-        answeroptions = [currentAnswer,genbluff(),genbluff(),genbluff()];
-    };
+    delete bluffAnswers.output;
+    return (output);
+};
 };
 
 
-function resetBluff(){
-     bluffAnswers =["jQuery","alse","Local Storage","function","keydown/keyup","if","else/else if",".querySelector","True","Array","Yes","for","script","Null","No","probably a cranberry","variable","Robot","Next Question"];
+function resetBluff() {
+    bluffAnswers = ["jQuery", "alse", "Local Storage", "function", "keydown/keyup", "if", "else/else if", ".querySelector", "True", "Array", "Yes", "for", "script", "Null", "No", "probably a cranberry", "variable", "Robot", "Next Question"];
 }
-function saveScore(){
+function saveScore() {
 
 }
-function viewScores(){
+function viewScores() {
 
 }
-function getOldScores(){
+function getOldScores() {
 
 }
 
-viewScoresbnt.addEventListener("click",viewScores());
-function lose(){
+viewScoresbnt.addEventListener("click", viewScores());
+function lose() {
     viewScores();
     var lostGame = document.createElement("h3");
     lostGame.textContent = "Sorry you lost. Try again";
-    playagain.appendChild (lostGame);
+    playagain.appendChild(lostGame);
 
 };
 
-function hideExtra(){
+function hideExtra() {
     para.setAttribute("style", "display: none");
     startButton.disabled = true;
     startButton.setAttribute("style", "display: none");
@@ -219,7 +219,7 @@ startButton.addEventListener("click", startGame);
 init();
 
 
-function playAgain(){
+function playAgain() {
     resetAray();
     para.setAttribute("style", "display: flex");
     startButton.disabled = false;
